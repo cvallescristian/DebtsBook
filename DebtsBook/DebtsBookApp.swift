a2f103dd9ed97217b32552318b7eaa00940331e8
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2026-07-09 11:41:09 +1200

    Initial Commit

---
+++ DebtsBook/DebtsBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebtsBookApp.swift
+//  DebtsBook
+//
+//  Created by Cristian on 09/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct DebtsBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
